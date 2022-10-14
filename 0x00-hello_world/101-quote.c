3f6381122d0a464560200ac5d6fcd4a7e552bb2b
kema-ray <rachelkemuma99@gmail.com>
2022-10-14 20:54:48 +0300

    Cproram tha prints standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,14 @@
 /**
  *main - Entry Point
  *Create Variables
- *Use write 
- *Use the sizeof operator 
+ *Use write
+ *Use the sizeof operator
  *Return: 1
  */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-    , sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	, sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
