1bf67a571b93e4bbfda9ccb06b1272d548db733c
kema-ray <rachelkemuma99@gmail.com>
2022-10-14 15:00:59 +0300

    Standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
+/**
+ *main - Entry Point
+ *Create Variables
+ *Use write 
+ *Use the sizeof operator 
+ *Return: 1
+ */
+
 int main(void)
 {
     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
