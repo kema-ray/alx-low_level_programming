d8cafc91062368ece485d80b0b96acfe4ac9def4
kema-ray <rachelkemuma99@gmail.com>
2022-10-16 16:19:31 +0300

    a program that prints the alphabet in lowercase using putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,15 +4,12 @@
  *main - Entry Point
  *Return: 0
  */
-int main()
+int main(void)
 {
-  
     char ch = 'a';
-  
     for (ch = 'a'; ch <= 'z'; ch++)
     {
         putchar(ch);
     }
-  
     return (0);
 }
