a8dfec245c12e472cf85371eb3909f6073112255
kema-ray <rachelkemuma99@gmail.com>
2022-10-19 00:53:35 +0300

    Do corrections to question 1 and 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,7 +9,7 @@ void print_alphabet(void)
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar('c');
+		_putchar(c);
 	}
 	_putchar('\n');
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 	while (count++ <= 9)
 	{
 		for (c = 'a'; c <= 'z'; c++)
-			_putchar('c');
+			_putchar(c);
 		_putchar('\n');
 	}
 }
