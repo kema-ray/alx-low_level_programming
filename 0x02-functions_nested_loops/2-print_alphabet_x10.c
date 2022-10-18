0f8113edcfbafeb38b6e9bd20fc860451fce31aa
kema-ray <rachelkemuma99@gmail.com>
2022-10-18 22:38:22 +0300

    function that prints 10 times the alphabet, in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,12 @@
 void print_alphabet_x10(void)
 {
 	int count = 0;
-	char letter;
+	char c;
 
 	while (count++ <= 9)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar('letter');
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar('c');
 		_putchar('\n');
 	}
 }
