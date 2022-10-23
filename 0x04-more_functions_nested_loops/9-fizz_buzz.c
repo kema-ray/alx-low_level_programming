98d7e8291d168fc12981d17f2054cb1d11c7f13f
kema-ray <rachelkemuma99@gmail.com>
2022-10-23 10:05:38 +0300

    program that prints the numbers from 1 to 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,15 @@ int main(void)
 	{
 		if (i % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz\n");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz\n");
 		}
 		else if (i % 15 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz\n");
 		}
 		else
 		{
