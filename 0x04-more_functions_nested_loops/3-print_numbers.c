aaaccc91c0885267c11aa291289a753172ba7774
kema-ray <rachelkemuma99@gmail.com>
2022-10-23 08:31:24 +0300

    function that prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * print_numbers - prints numbers from 0 to 9
+  * Return: 0
+  */
+
+void print_numbers(void)
+{
+	char c = '0';
+
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
