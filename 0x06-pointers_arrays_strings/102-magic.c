179b65dd5475a0051176adfef113c70f1f39f039
kema-ray <rachelkemuma99@gmail.com>
2022-10-29 22:11:24 +0300

    magic file

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *main - Entry Point
  *Return: 0
@@ -20,8 +21,8 @@ int main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-		*(p + 5) = 98;
-/* ...so that this prints 98\n */
+		*(p+5) = 98;
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
