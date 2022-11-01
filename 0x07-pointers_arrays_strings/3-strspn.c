17b58d0036d749b7b430f69d634eab634d517ce4
mutua <mutua.mutinda@valuechainfactory.com>
2022-11-01 16:10:21 +0300

    function that gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,10 +17,12 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0' && accept[j] != s[i]; j++)
+		{
 			if (s[i] == accept[j])
 			length++;
 			if (accept[j] == '\0')
 			return (length);
+		}
 	}
 	return (length);
 }
