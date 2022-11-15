55cbdcadf5071b3e46b2b479e100beee4adcd103
kema-ray <rachelkemuma99@gmail.com>
2022-11-15 19:01:59 +0300

    function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,6 +4,7 @@
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
+ *
  * Return: the length of the string
  */
 int _strlen(char *s)
@@ -26,6 +27,7 @@ int _strlen(char *s)
  * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
  * @src: string to be copied
+ *
  * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
@@ -53,6 +55,7 @@ char *_strcpy(char *dest, char *src)
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
+ *
  * Return: pointer to the new dog (Success), NULL otherwise
  */
 dog_t *new_dog(char *name, float age, char *owner)
