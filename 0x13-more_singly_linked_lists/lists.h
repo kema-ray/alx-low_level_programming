6128cdccde8d4b02a7a1ae8c02697add231610ee
kema-ray <rachelkemuma99@gmail.com>
2022-12-01 08:20:30 +0300

    function that adds a new node at the end of a listint_t list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,5 +21,5 @@ typedef struct listint_s
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 size_t print_listint(const listint_t *h);
-
+listint_t *add_nodeint_end(listint_t **head, const int n);
 #endif
