27ef566909c653dbecbf64b5fb901b5e7598d8e1
kema-ray <rachelkemuma99@gmail.com>
2022-12-01 08:37:16 +0300

    function that frees a listint_t list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -22,4 +22,5 @@ listint_t *add_nodeint(listint_t **head, const int n);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
 #endif
