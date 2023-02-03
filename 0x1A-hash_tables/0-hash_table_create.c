637be3be8f2920297cc08301f3ca2af7ce131faf
kema-ray <rachelkemuma99@gmail.com>
2023-02-03 17:12:22 +0300

    function that creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,7 +4,7 @@
   * hash_table_create - creates a hash table
   * @size: size of an array
   * Return: a pointer to the newly created hash table
-  *	    or NULL if something went wrong
+  *	        or NULL if something went wrong
   */
 
 hash_table_t *hash_table_create(unsigned long int size)
